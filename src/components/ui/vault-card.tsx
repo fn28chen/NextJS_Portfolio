d775c086ce660ab22833d539abe76592ec9b7258
phong28zk <lamperougelelouchanm@gmail.com>
2024-07-31 10:16:51 +0700

    Fix: Fix UI vault-card

--- src/components/ui/vault-card.tsx
+++ src/components/ui/vault-card.tsx
@@ -39,7 +39,7 @@ export const EvervaultCard = ({
     >
       <div
         onMouseMove={onMouseMove}
-        className="group/card rounded-2xl w-full relative overflow-hidden bg-transparent flex items-center justify-center h-full"
+        className="group/card w-full relative overflow-hidden bg-transparent flex items-center justify-center h-full"
       >
         
         <div className="relative z-10 flex items-center justify-center">
@@ -59,7 +59,7 @@ export function CardPattern({ mouseX, mouseY, randomString }: any) {
 
   return (
     <div className="pointer-events-none">
-      <div className="absolute inset-0 rounded-2xl  [mask-image:linear-gradient(white,transparent)] group-hover/card:opacity-50"></div>
+      <div className="absolute inset-0 [mask-image:linear-gradient(white,transparent)] group-hover/card:opacity-50"></div>
       <motion.div
         className="absolute inset-0 rounded-2xl bg-gradient-to-r from-green-500 to-blue-700 opacity-0  group-hover/card:opacity-100 backdrop-blur-xl transition duration-500"
         style={style}
