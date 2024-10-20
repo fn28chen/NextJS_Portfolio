81c68664b0aee69b25a5a1a675034f3872f3c96d
phong28zk <lamperougelelouchanm@gmail.com>
2024-10-20 07:32:05 +0700

    fix/hotfix: describe

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,81 +1,89 @@
 export const info = [
-    {
-      title: `Detail Information`,
-      description: `I am a Front-End Developer with 1 years of experience. I have
+  {
+    title: `Detail Information`,
+    description: `I am a Front-End Developer with 1 years of experience. I have
       experience in building responsive websites and web applications.
       I am passionate about building excellent software that improves
       the lives of those around me.`,
-    },
-    {
-      title: `Education`,
-      description: `Now I am a 4-year student at the Post Telecommunication Institute of Technology. My major is Electronic Telecommunications. I have a top-class GPA with top 1% of the school.`,
-    },
-    {
-      title: `Career Objective`,
-      description: `Now I work as a freelancer Front-End Developer. I am looking for a job. Addition, I work part-time as a channel leader of Discord HLV Online - that's a channel with content around football analysis and tactics.`,
-    },
-    {
-      title: `More...`,
-      description: `I'm interested in something new like T3-stack, shadcnUI, trpc or something else. I want to learn new things whenever my schedule allow me to do this.`,
-    },
-  ];
-  
-  export const Projects = [
-    {
-      title: "P28ZK Gallery",
-      href: `https://p28zk-gallery.vercel.app`,
-      description: `This project allows user to upload their images to the server and view them in the gallery. It's use NextJS, TailwindCSS, ShadCN, TypeScript, PostgreSQL and Drizzle as a full-stack framework.`,
-      techStack: [
-        `NextJS`,
-        `TailwindCSS`,
-        `ShadCN`,
-        `TypeScript`,
-        `Drizzle`,
-        `PostgreSQL`,
-      ],
-    },
-    {
-      title: `Thang Vu Blogs`,
-      href: `hhttps://thang-vu-blogs.vercel.app/`,
-      description: `A portfolio a.k.a a blog for my brother. It use many good things like Sanity with GROQs to create, edit and query data, NextJS with TailwindCSS to build the front-end and TypeScript to type-check the code.`,
-      techStack: [`NextJS`, `TailwindCSS`, `TypeScript`, `Sanity`],
-    },
-    {
-      title: `T3 Spammer Emoji`,
-      href: `https://t3-git.vercel.app/`,
-      description: `This project is a funny, chill project I did. User can chat with others with emoji, to perform their feeling. It's use NextJS, TailwindCSS, ShadCN, TypeScript and T3 as a full-stack framework.`,
-      techStack: [`NextJS`, `TailwindCSS`, `ShadCN`, `TypeScript`, `Prisma`, `PostgreSQL`, `T3`],
-    },
-    {
-      title: `Reading List`,
-      href: `https://reading-list-opal.vercel.app/`,
-      description: `That's my reading list with API from open-library. It's use NextJS, TailwindCSS, TypeScript and shadcnUI as Front-end, axios as API client and use open-library API as a data source.`,
-      techStack: [`NextJS`, `TailwindCSS`, `TypeScript`, `ShadCN`],
-    },
-    {
-      title: `Blog`,
-      href: `https://blog-ketket.vercel.app/`,
-      description: `This is a blog build for Facebook Page for their 12 Cung Hoang Dao Series. It is a blog that allows users to read their daily horoscope and also allows users to read their daily fortune.`,
-      techStack: [
-        `NextJS`,
-        `TailwindCSS`,
-        `FramerMotion`,
-        `Prisma`,
-        `MySQL`,
-        `Sanity`,
-      ],
-    },
-    {
-      title: `Pro Chat`,
-      href: `https://oversimplifiedteam7.netlify.app/`,
-      description: `Pro Chat is a group project that I built in the 5th semester with my collaborators. It is a chat application that allows users to chat with each other in real-time.`,
-      techStack: [
-        `ReactJS`,
-        `TailwindCSS`,
-        `TypeScript`,
-        `Python`,
-        `SocketIO`,
-        `MongoDB`,
-      ],
-    },
-  ];
+  },
+  {
+    title: `Education`,
+    description: `I am currently a fourth-year student at the Post Telecommunication Institute of Technology, specializing in Electronic Telecommunications. I have maintained an exceptional GPA of 3.43 out of 4.0, ranking in the top 1% of my department. Throughout my academic journey, I have been awarded semester scholarships six times, reflecting my dedication to excellence and my strong commitment to my field of study.`,
+  },
+  {
+    title: `Career Objective`,
+    description: `I am currently working as a Front-End Developer at Tinasoft JSC, where I contribute to creating dynamic and user-friendly web applications. In addition to my professional responsibilities, I am dedicated to continuous improvement and skill enhancement through various solo projects that allow me to explore new technologies and refine my expertise.`,
+  },
+  {
+    title: `More...`,
+    description: `I'm passionate about exploring new technologies such as the T3 stack, shadcnUI, and tRPC. I strive to learn and grow whenever my schedule permits. In my downtime, I enjoy listening to classical music - especially Mozart, Verdi (Requiem? I don't know I write it right for this...) and meditation tracks, along with savoring a good cup of tea.`,
+  },
+];
+
+export const Projects = [
+  {
+    title: "P28ZK Gallery",
+    href: `https://p28zk-gallery.vercel.app`,
+    description: `This project allows user to upload their images to the server and view them in the gallery. It's use NextJS, TailwindCSS, ShadCN, TypeScript, PostgreSQL and Drizzle as a full-stack framework.`,
+    techStack: [
+      `NextJS`,
+      `TailwindCSS`,
+      `ShadCN`,
+      `TypeScript`,
+      `Drizzle`,
+      `PostgreSQL`,
+    ],
+  },
+  {
+    title: `Thang Vu Blogs`,
+    href: `hhttps://thang-vu-blogs.vercel.app/`,
+    description: `A portfolio a.k.a a blog for my brother. It use many good things like Sanity with GROQs to create, edit and query data, NextJS with TailwindCSS to build the front-end and TypeScript to type-check the code.`,
+    techStack: [`NextJS`, `TailwindCSS`, `TypeScript`, `Sanity`],
+  },
+  {
+    title: `T3 Spammer Emoji`,
+    href: `https://t3-git.vercel.app/`,
+    description: `This project is a funny, chill project I did. User can chat with others with emoji, to perform their feeling. It's use NextJS, TailwindCSS, ShadCN, TypeScript and T3 as a full-stack framework.`,
+    techStack: [
+      `NextJS`,
+      `TailwindCSS`,
+      `ShadCN`,
+      `TypeScript`,
+      `Prisma`,
+      `PostgreSQL`,
+      `T3`,
+    ],
+  },
+  {
+    title: `Reading List`,
+    href: `https://reading-list-opal.vercel.app/`,
+    description: `That's my reading list with API from open-library. It's use NextJS, TailwindCSS, TypeScript and shadcnUI as Front-end, axios as API client and use open-library API as a data source.`,
+    techStack: [`NextJS`, `TailwindCSS`, `TypeScript`, `ShadCN`],
+  },
+  {
+    title: `Blog`,
+    href: `https://blog-ketket.vercel.app/`,
+    description: `This is a blog build for Facebook Page for their 12 Cung Hoang Dao Series. It is a blog that allows users to read their daily horoscope and also allows users to read their daily fortune.`,
+    techStack: [
+      `NextJS`,
+      `TailwindCSS`,
+      `FramerMotion`,
+      `Prisma`,
+      `MySQL`,
+      `Sanity`,
+    ],
+  },
+  {
+    title: `Pro Chat`,
+    href: `https://oversimplifiedteam7.netlify.app/`,
+    description: `Pro Chat is a group project that I built in the 5th semester with my collaborators. It is a chat application that allows users to chat with each other in real-time.`,
+    techStack: [
+      `ReactJS`,
+      `TailwindCSS`,
+      `TypeScript`,
+      `Python`,
+      `SocketIO`,
+      `MongoDB`,
+    ],
+  },
+];
